bbb72faa88bdae4b61fea4d3d8d3688bca108dd1
ramanks19 <ramans.91@rediffmail.com>
2023-05-14 02:27:11 +0530

    Changes for Eureka Client

--- user_service/src/main/java/com/hotel/rating/service/user_service/UserServiceApplication.java
+++ user_service/src/main/java/com/hotel/rating/service/user_service/UserServiceApplication.java
@@ -2,8 +2,10 @@ package com.hotel.rating.service.user_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
