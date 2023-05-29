f71ae281e6e4ad065ba41e4c346ff4f2871ffed4
ramanks19 <ramans.91@rediffmail.com>
2023-05-29 23:42:14 +0530

    Security Implementation Using OKTA

--- rating_service/src/main/java/com/hotel/rating/service/rating_service/controller/RatingController.java
+++ rating_service/src/main/java/com/hotel/rating/service/rating_service/controller/RatingController.java
@@ -7,6 +7,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -24,26 +25,31 @@ public class RatingController {
     @Autowired
     private RatingService ratingService;
     
+    @PreAuthorize("hasAuthority('Admin')")
     @PostMapping
     public ResponseEntity<Rating> createRating(@Valid @RequestBody Rating rating) {
         return new ResponseEntity<>(ratingService.createRating(rating), HttpStatus.CREATED);
     }
 
+    @PreAuthorize("hasAuthority('SCOPE_internal')")
     @GetMapping("/all")
     public ResponseEntity<List<Rating>> getAllRatings() {
         return new ResponseEntity<>(ratingService.getAllRatings(), HttpStatus.OK);
     }
 
+    @PreAuthorize("hasAuthority('SCOPE_internal') || hasAuthority('Admin')")
     @GetMapping("/users/{userId}")
     public ResponseEntity<List<Rating>> findByUserId(@PathVariable Long userId) {
         return new ResponseEntity<>(ratingService.getRatingByUserId(userId), HttpStatus.OK);
     }
 
+    @PreAuthorize("hasAuthority('Admin')")
     @GetMapping("/hotels/{hotelId}")
     public ResponseEntity<List<Rating>> findByHotelId(@PathVariable Long hotelId) {
         return new ResponseEntity<>(ratingService.getRatingsByHotelId(hotelId), HttpStatus.OK);
     }
 
+    @PreAuthorize("hasAuthority('Admin')")
     @GetMapping("/users/{userId}/hotels/{hotelId}")
     public ResponseEntity<List<Rating>> findByUserIdAndHotelId(@PathVariable Long userId, @PathVariable Long hotelId) {
         return new ResponseEntity<>(ratingService.getRatingsByUserIdAndHotelId(userId, hotelId), HttpStatus.OK);
